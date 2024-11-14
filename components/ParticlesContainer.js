09a94cc4274defe2bb9adb172ff0b1e0493ff9ff
Denis Mugambi Muthuri <mugambimuthuri0@gmail.com>
2024-11-14 08:55:26 +0300

    "Updated components and pages with new code, added imports, and modified JSX elements"

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,4 +1,9 @@
+import {Particles} from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import React, {useCallback} from 'react';
+
 const ParticlesContainer = () => {
+  // init 
   return <div>Particles Container</div>;
 };
 
